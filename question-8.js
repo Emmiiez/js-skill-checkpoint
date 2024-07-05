37743c664cc83afb811b48def550a1a065f36e8b
Emmiiez <emmiiez.m@gmail.com>
2024-07-05 12:25:08 +0700

    Update question-8.js

--- question-8.js
+++ question-8.js
@@ -1,3 +1,23 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+let Jsonplacehokder = () => {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+};
+
+const handleResponse = (response) => {
+    return response.json();
+};
+
+const onSucess = (data) => {
+    const newUsers = data.map(user => user.name);
+    console.log(newUsers);
+};
+
+let onFailure = (error) => {
+    console.error(error);
+};
+Jsonplacehokder ()
+.then(handleResponse)
+.then(onSucess)
+.catch(onFailure);
